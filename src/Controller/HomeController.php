2fc19b2fb7509543165d796e138462f3c05bc500
Jasmin Medved <jmedved85@gmail.com>
2025-02-18 09:41:19 +0100

    home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,11 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DashboardController extends AbstractController
+class HomeController extends AbstractController
 {
-    #[Route('/dashboard', name: 'app_dashboard')]
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('dashboard/index.html.twig');
+        return $this->render('home/index.html.twig');
     }
 }
